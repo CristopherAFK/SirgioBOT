c04ce03e9c54fc37174dd18bb5fa5f05189475fb
CristopherAFK <garscraftgg@gmail.com>
2025-10-30 21:44:46 -0600

    Update channel ID for autoroles command

--- autoroles.js
+++ autoroles.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Envía los mensajes de autoroles.",
   async execute(message) {
     const allowedRole = "1229140504310972599";
-    const channelID = "1422713049957273621";
+    const channelID = "1212998804467617863";
 
     if (!message.member.roles.cache.has(allowedRole))
       return message.reply("❌ No tienes permiso para usar este comando.");
